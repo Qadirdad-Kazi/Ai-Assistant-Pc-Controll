200274331dd51ad8274901b1b093bf4ed3674631
Qadirdad-Kazi <154679341+Qadirdad-Kazi@users.noreply.github.com>
2025-10-03 19:04:59 +0500

    removed

--- mic_test.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-
-print("Microphones found:", sr.Microphone.list_microphone_names())
-try:
-    with sr.Microphone() as mic:
-        print("Microphone is accessible!")
-except Exception as e:
-    print(f"Microphone access error: {e}") 
